bc5b2de0e4288e97e60d22e400ba3c8e64139b79
Saanykeys <125324312+Saanykeys@users.noreply.github.com>
2023-03-01 11:06:01 -0500

    Add files via upload

---
+++ Exercises.py
@@ -0,0 +1,12 @@
+class Employee:
+    
+    def__init__(self, first, last, title, salary):
+        self.first_name = first
+        self.last_name = last
+        self.job.title = title
+        self.salary = salary
+        self. email = first[0].lower()+last.lower()+last.lower()
+        
+    def apply.raise(self)
+        self.salary +- Employee Raise AMount 
+
